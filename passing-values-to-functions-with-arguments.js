5f6806ec9af5749f5a1d9a114978f17d1ea42c77
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2022-11-27 13:14:48 -0300

    +exercises

---
+++ passing-values-to-functions-with-arguments.js
@@ -0,0 +1,5 @@
+function functionWithArgs(n1, n2) {
+  console.log(n1 + n2);
+}
+
+functionWithArgs(1, 2);
---
+++ shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [];
+
+myList.push(["candy", 7]);
+myList.push(["chocolate", 5]);
+myList.push(["biscuit", 9]);
+myList.push(["soft drink", 12]);
+myList.push(["water", 10]);
