7afefd5be244e63191c79b25ef1a24466cd1f4ef
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2022-11-30 06:47:32 -0300

    solve manipulating-complex-objects

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Legiao Urbana",
+    "title": "Dois",
+    "release_year": 1985,
+    "formats": [
+      "CD",
+      "LP",
+      "DVD"
+    ]
+  }
+];
